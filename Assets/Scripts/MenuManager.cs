d6ab6eedd2fd9fdbc69d67694215c153a1bafb9c
Martin Zackrisson <martin.zackrisson@fripost.org>
2021-10-04 18:54:07 +0200

    Updated shop

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public void Play() 
+    {
+        IntermissionNarrator.Stage = 0;
+        SceneManager.LoadScene("Intermission");
+    }
+}
